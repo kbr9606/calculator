ab3edd5c45875f41b9122acb2204ecb6c50b5501
DESKTOP-IPCCTPD\User <kbr9606@gmail.com>
2021-02-11 03:16:17 +0900

    gui 계산기 구현하기 3

--- calc-gui.js
+++ calc-gui.js
@@ -1,62 +1,39 @@
+//입력을 담당하는 객체
 var input = {"array" : []};
 
-
+//입력받은 수식을 문자열로 리턴
 input.getInput = function() {
     return this.array.join("");
-}
-
-var output = {};
-output.text = document.getElementById("output");
-
-var clickNumbers = function(event) {
-    var str = event.target.innerHTML;
-    console.log(str);
-
-    if (str === "BS") {
-        input.array.pop();
-    }
-    else if (str === "+" || str === "-" || str === "*" || str === "/") {
-        input.array.push(" " + str + " ");
-    }
-    else {
-        input.array.push(str);
-    }
-    
-    
-    if (input.array.length == 0) {
-        output.text.innerHTML = "Empty";
-    }
-    else {
-    output.text.innerHTML = input.getInput();
-    }
-}
-
-
-
-var showResult = function(event) {
-    console.log(event.target.innerHTML);
-}
-
-
-
-/*var input = {};
-
-input.init = function(str) {
-    this.list = str.split(" ");
 };
 
-input.empty = function() {
-    return this.list.length == 0;
-}
+//입력 배열을 초기화
+input.removeAll = function(value) {
+    this.array = [];
+    this.array.push(value);
+};
 
+//수식이 비었는지 검사
+input.empty = function() {
+    return this.array.length === 0;
+};
+
+//계산을 실행하기 전 준비 단계
+//getValue()를 호출하기 전 반드시 수행되어야 함
+input.prepareCalc = function() {
+    this.array = this.array.join("").split(" ");
+};
+
+//수식에서 값을 읽어옴
 input.getValue = function() {
-    var str = this.list.shift();
+    var str = this.array.shift();
     var n = Number(str);
     return n;
 };
 
+
+//수식에서 연산자를 읽어옴
 input.getOperator = function() {
-    var op = this.list.shift();
+    var op = this.array.shift();
     if (op === "+" || op === "-" || op === "*" || op === "/") {
         return op;
     }
@@ -65,7 +42,24 @@ input.getOperator = function() {
     }
 };
 
-calculator = {};
+//output 객체
+//출력을 담당
+var output = {};
+output.text = document.getElementById("output");
+
+//계산 결과를 출력
+output.print = function(str) {
+    this.text.innerHTML = str;
+}
+
+//수식을 출력
+output.display = function() {
+    this.text.innerHTML = input.getInput();
+}
+
+//calculator 객체
+//계산을 담당
+var calculator = {};
 calculator.calculate = function(first, second, op) {
     var ret;
     switch(op) {
@@ -87,22 +81,45 @@ calculator.calculate = function(first, second, op) {
     return ret;
 }
 
-var output = {};
-output.put = document.getElementById("output");
+//숫자 버튼의 핸들러 
+var clickNumbers = function(event) {
+    var str = event.target.innerHTML;
+    console.log(str);
 
-output.print = function(value) {
-    this.put.innerHTML = "최종 결과값은 " + value + "입니다.";
+    if (str === "BS") {
+        input.array.pop();
+    }
+    else if (str === "+" || str === "-" || str === "*" || str === "/") {
+        input.array.push(" " + str + " ");
+    }
+    else {
+        input.array.push(str);
+    }
+
+    
+    if (input.empty()) {
+        output.text.innerHTML = "Empty";
+    }
+    else {
+        output.display();
+    }
 };
 
-function calc() {
-    var str = document.getElementById("input").value;
-    input.init(str);
+//= 버튼의 핸들러 
+var showResult = function(event) {
+    input.prepareCalc();
     var result = input.getValue();
     while (!input.empty()) {
         var op = input.getOperator();
         var second = input.getValue();
         result = calculator.calculate(result, second, op);
-    }
-
+    }    
     output.print(result);
-} */
+    input.removeAll(result);
+    
+};
+
+
+
+
+
